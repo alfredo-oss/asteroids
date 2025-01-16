1a3674a3e971de904e7009f3999a3e9c17ee461e
alfredo-oss <alfredo.derodt@gmail.com>
2025-01-16 18:46:35 -0300

    D: added drawing of player to the screen

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from constants import *
+from player import Player
 import pygame
 
 def main():
     pygame.init()
+    # defining initial player position vector
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y, PLAYER_RADIUS)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,6 +20,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         mls = time.tick(60)
         dt = mls/1000

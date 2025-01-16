21a59b585cdc920d775920325fc4a288a0e808fc
alfredo-oss <alfredo.derodt@gmail.com>
2025-01-16 17:15:43 -0300

    C: added limitation to the frame rate inside the game loop to 60 fps

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    time = pygame.time.Clock()
+    dt = 0
     #defining the game-loop
     while True:
         for event in pygame.event.get():
@@ -15,6 +16,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
-
+        mls = time.tick(60)
+        dt = mls/1000
 if __name__ == "__main__":
     main()
